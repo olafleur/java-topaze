d412cbd1d3edffc3718cd4157b3cf0a7728eb047
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-03-11 11:31:12 -0400

    Ajouter premier test.

--- src/main/java/com/olivierlafleur/topaze/App.java
+++ src/main/java/com/olivierlafleur/topaze/App.java
@@ -1,9 +1,5 @@
 package com.olivierlafleur.topaze;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/test/java/com/olivierlafleur/topaze/NodeTest.java
@@ -0,0 +1,12 @@
+package com.olivierlafleur.topaze;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class NodeTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
