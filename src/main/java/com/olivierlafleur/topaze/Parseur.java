42f672ef4e82289510a1cee8162bdef5bd2bcfde
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-03-17 21:29:14 -0400

    Réduire à un test.

--- src/main/java/com/olivierlafleur/topaze/Parseur.java
+++ src/main/java/com/olivierlafleur/topaze/Parseur.java
@@ -1,10 +1,19 @@
 package com.olivierlafleur.topaze;
 
-import com.olivierlafleur.topaze.instructions.Instruction;
-import com.olivierlafleur.topaze.instructions.InstructionInitialisation;
+import org.antlr.v4.runtime.*;
+
+import java.io.IOException;
 
 public class Parseur {
-    public Instruction parse(String s) {
-        return new InstructionInitialisation();
+    public void parse(String chemin) throws IOException {
+        TopazeLexer l = new TopazeLexer(new ANTLRInputStream(getClass().getResourceAsStream(chemin)));
+        TopazeParser p = new TopazeParser(new CommonTokenStream(l));
+        p.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                throw new IllegalStateException("failed to parse at line " + line + " due to " + msg, e);
+            }
+        });
+        p.topaze();
     }
 }
--- src/test/java/com/olivierlafleur/topaze/ParseurTest.java
+++ src/test/java/com/olivierlafleur/topaze/ParseurTest.java
@@ -1,38 +1,16 @@
 package com.olivierlafleur.topaze;
 
-import com.olivierlafleur.topaze.instructions.Instruction;
-import com.olivierlafleur.topaze.instructions.InstructionInitialisation;
-import org.antlr.v4.runtime.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
 
 public class ParseurTest {
     @Test
-    public void testParserInitialisation() {
+    public void testLectureFichierSimple() throws IOException {
         Parseur parseur = new Parseur();
 
-        Instruction instruction = parseur.parse("x vaut 3.");
+        parseur.parse("/test.tpz");
 
-        assertTrue(instruction instanceof InstructionInitialisation);
-
-        InstructionInitialisation instructionInitialisation = (InstructionInitialisation) instruction;
-
-        assertEquals(3, instructionInitialisation.getValeur());
-        assertEquals("x", instructionInitialisation.getNom());
-    }
-
-    @Test
-    public void testExampleField() throws Exception {
-        TopazeLexer l = new TopazeLexer(new ANTLRInputStream(getClass().getResourceAsStream("/test.tpz")));
-        TopazeParser p = new TopazeParser(new CommonTokenStream(l));
-        p.addErrorListener(new BaseErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-                throw new IllegalStateException("failed to parse at line " + line + " due to " + msg, e);
-            }
-        });
-        p.topaze();
+        //TODO : Valider valeur stockée
     }
 }
