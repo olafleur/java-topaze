287e082982c650bf97cd916e3e54cf1a73b569bb
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-05-12 15:07:49 -0400

    Bien stocker une chaîne dans la machine

--- src/test/java/com/olivierlafleur/topaze/InitialisationChaineTest.java
+++ src/test/java/com/olivierlafleur/topaze/InitialisationChaineTest.java
@@ -4,11 +4,22 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
 public class InitialisationChaineTest {
     @Test
     public void testParseInitialisationChaine() throws IOException {
         Parseur parseur = new Parseur();
 
         parseur.executerInstruction(new EtatMachine(), "nom vaut \"Olivier Lafleur\".");
+    }
+
+    @Test
+    public void testAjoutChaineMachine() {
+        EtatMachine etatMachine = new EtatMachine();
+
+        etatMachine.ajouterVariable(new Variable("nom", "Olivier Lafleur"));
+
+        assertEquals("Olivier Lafleur", etatMachine.getValeur("nom").getValeurChaine());
     }
 }
